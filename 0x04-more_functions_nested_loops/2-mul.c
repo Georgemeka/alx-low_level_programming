a8803ebfe1db60361fba7f5c58a7da2561406052
George Iheanyi <georgeiheanyi@gmail.com>
2022-12-17 14:01:22 -0800

    Update with Multiplication task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer variable 1
+ * @b: integer variable 2
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
