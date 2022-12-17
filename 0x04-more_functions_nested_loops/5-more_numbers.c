6b646e88e384db7209a70e11493795870c20fe4f
George Iheanyi <georgeiheanyi@gmail.com>
2022-12-17 15:23:03 -0800

    Update with task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14
+ * _putchar - prints charcter to stdout
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+			_putchar(j / 10 + '0');
+			}
+		}
+		_putchar(j % 10 + '0');
+	}
+	_putchar('\n');
+}
