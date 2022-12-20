05f0b01d355f985ba47f0b759f18bfba024384c9
George Iheanyi <georgeiheanyi@gmail.com>
2022-12-20 08:41:08 -0800

    Pointers task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer and updates value
+ * _putchar - prints to standard output
+ * @n: variable pointer n
+ * Return: always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
