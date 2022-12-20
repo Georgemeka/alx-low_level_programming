697ff71be96a481f485dd8a3cc3865c202083e55
George Iheanyi <georgeiheanyi@gmail.com>
2022-12-20 09:13:15 -0800

    String length task 3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: variable s
+ * Return: Always 0;
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+	}
+	return (i);
+}
