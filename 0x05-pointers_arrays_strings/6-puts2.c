66148b90ecacf8c30eda15fe5d17532aa52a0802
George Iheanyi <georgeiheanyi@gmail.com>
2022-12-20 15:20:23 -0800

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prits characters of a string
+ * @str: variable named str
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
