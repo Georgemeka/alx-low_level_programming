3b8e9cf4794aa1b0500ce1d33382b201bb54150d
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-04 12:43:12 -0800

    Recursion task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: variable input
+ */
+
+void _puts_recursion(char *s)
+{
+	char *s1 = s;
+
+	if (*s1 != '\0')
+	{
+		_putchar(*s1);
+		s1++;
+		_puts_recursion(s1);
+	}
+	else 
+		_putchar('\n');
+}
