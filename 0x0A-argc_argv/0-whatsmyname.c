c88781d44fd783df73af63b688969456b479ed06
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-07 08:39:50 -0800

    Project to determine name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - returns the name of the file
+ * @argc:  argument count
+ * @argv: argument vector
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
