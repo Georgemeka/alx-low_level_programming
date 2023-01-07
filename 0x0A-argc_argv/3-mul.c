7a3a88cbc6f7f5750a7f8448fb471a5dc405827e
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-07 09:35:47 -0800

    Update with 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if successful, 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", mul);
+	return (0);
+}
