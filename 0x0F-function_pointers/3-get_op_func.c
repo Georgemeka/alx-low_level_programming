7e148db3e14df9db31b61b97be337e4f6122640f
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-19 16:29:05 -0800

    Pointer Function Tasks

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - compare string and return appropriate opeartor function
+ * @s: operator characters (e.g. '+')
+ * Return: returns the result of function
+ */
+
+
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+
+/**
+ * op_add - returns sum of a an b
+ * @a: 1st parameter of function
+ * @b: 2nd parameter of function
+ * Return: sum of numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds modulus of two numbers
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
