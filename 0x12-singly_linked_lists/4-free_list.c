dae4f338c97ad2eeab30956ddee02e621dab7955
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-27 21:39:40 -0800

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees list 
+ * @head: head list that represents whole list ot be freed
+ * Return: freed list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *hold;
+
+	while (head != NULL)
+	{
+		hold = head;
+		head = head->next;
+		free(hold->str);
+		free(hold);
+	}
+}
