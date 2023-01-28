24236280349e069a5a236807a9ce40c63f8b7035
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-27 22:04:23 -0800

    Print function before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+void print_words(void) __attribute__((constructor));
+
+/**
+ * print_words - prints words before main is executed
+ */
+
+void print_words(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
