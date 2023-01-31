206492877c9d99b01f2eedfc8adf12fa37197cb4
George Iheanyi <georgeiheanyi@gmail.com>
2023-01-31 11:38:07 -0800

    Free list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
+	if (!head)
 		return;
 
-	while (*head != NULL)
+	while (*head)
 	{
-		temp = *head;
+		temp = (*head)->next;
 		free(*head);
-		*head = temp->next;
+		*head = temp;
 	}
-	*head = NULL;
 }
